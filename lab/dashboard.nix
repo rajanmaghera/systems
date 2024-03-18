fe1b66d9603a8b8cd8ac20b7ccfb2e3b3219092a
Rajan Maghera <rmaghera@ualberta.ca>
2024-03-18 16:25:51 -0400

    feat: customize dashboard

--- lab/dashboard.nix
+++ lab/dashboard.nix
@@ -9,12 +9,56 @@ with lib; {
   # Add an entry to the dashboard for the service
   config.services.homepage-dashboard = mkIf config.lab.enable {
     enable = true;
+    settings = {
+      title = config.networking.hostName;
+      headerStyle = "clean";
+    };
+
+    widgets = [
+      {
+        search = {
+          provider = "google";
+          focus = true;
+          showSearchSuggestions = true;
+          target = "_blank";
+        };
+      }
+      {
+        resources = {
+          cpu = true;
+          memory = true;
+          disk = "/";
+          cputemp = true;
+          uptime = true;
+          units = "metric";
+          refresh = 3000;
+          diskUnits = "bytes";
+        };
+      }
+    ];
 
     services =
-      attrsets.mapAttrsToList
-      (name: value: {
-        "${value.inp.category}" = [{"${value.inp.fullName}" = {href = "${value.url}";};}];
-      })
-      config.lab.details;
+      (attrsets.mapAttrsToList
+        (name: value: {
+          "${value.inp.category}" = [{"${value.inp.fullName}" = {href = "${value.url}";};}];
+        })
+        config.lab.details)
+      ++ [
+        {
+          "Server" = [
+            {
+              "Caddy" = {
+                href = "http://localhost:2019";
+                description = "Reverse proxy";
+                icon = "caddy.png";
+                widget = {
+                  type = "caddy";
+                  url = "http://localhost:2019";
+                };
+              };
+            }
+          ];
+        }
+      ];
   };
 }
