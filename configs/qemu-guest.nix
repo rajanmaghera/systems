2f014fa05d989c5a77a976ced11e36afcf17979b
Rajan Maghera <rmaghera@ualberta.ca>
2024-07-01 19:42:10 -0400

    feat: add shared folder support for QEMU guest

--- configs/qemu-guest.nix
+++ configs/qemu-guest.nix
@@ -13,11 +13,40 @@ in {
       default = false;
       description = "Enable configuration for a QEMU guest with video drivers.";
     };
+    sharedFolder = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable shared folder via VirtFS. The permissions of the shared folder might not be correct. To fix this, run `sudo fix-virtfs-permissions`.";
+    };
   };
 
   config = mkIf cfg.enable {
     services.qemuGuest.enable = true;
     services.spice-vdagentd.enable = true;
     services.xserver.videoDrivers = ["qxl"];
+
+    system.activationScripts.makeMountFolderForVirtFs = mkIf cfg.sharedFolder {
+      text = ''
+        mkdir -p /mnt/virtfs
+      '';
+    };
+
+    fileSystems."/mnt/virtfsroot" = mkIf cfg.sharedFolder {
+      device = "share";
+      fsType = "9p";
+      options = [
+        "trans=virtio"
+        "version=9p2000.L"
+        "rw"
+        "_netdev"
+        "nofail"
+      ];
+    };
+
+    environment.systemPackages = mkIf cfg.sharedFolder [
+      (pkgs.writeScriptBin "fix-virtfs-permissions" ''
+        chown -R rajan:users /mnt/virtfs
+      '')
+    ];
   };
 }
