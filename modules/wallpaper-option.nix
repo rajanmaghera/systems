b6e34d4f51b01799fd0f7208c0776cdcb8170d81
Rajan Maghera <rmaghera@ualberta.ca>
2024-07-03 23:32:24 -0400

    refactor: push theme config out to JSON

--- modules/wallpaper-option.nix
+++ modules/wallpaper-option.nix
@@ -4,9 +4,10 @@
   ...
 }:
 with lib; let
+  baseConfg = builtins.fromJSON (builtins.readFile ../configuration.json);
   source = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/rajanmaghera/systems/wallpaper/austin-schmid-mZSRmKfhIkM-unsplash.jpg";
-    sha256 = "sha256-CycIembvGpjRcv0+F2ThtGHrtze9waz2emmJAtxIVok=";
+    url = baseConfg.wallpaper.url;
+    sha256 = baseConfg.wallpaper.sha256;
   };
 in {
   options.my.wallpaper = {
