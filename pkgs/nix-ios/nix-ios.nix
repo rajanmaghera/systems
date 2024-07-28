bdf842b7ed226ffceccfeebef091cfcbaeaf9833
Rajan Maghera <rmaghera@ualberta.ca>
2024-07-28 15:13:11 -0400

    chore: format nix-ios

--- pkgs/nix-ios/nix-ios.nix
+++ pkgs/nix-ios/nix-ios.nix
@@ -1,4 +1,8 @@
-{imessage-ruby, writeShellScriptBin, ...}:
+{
+  imessage-ruby,
+  writeShellScriptBin,
+  ...
+}:
 writeShellScriptBin "nix-ios" ''
   OUTPUT_CONFIG=$(nix eval .#iosConfigurations.$1.output | sed 's@\\@@g' | sed -e 's/^"//' -e 's/"$//')
   echo "$OUTPUT_CONFIG" > ~/Pictures/ios_config.json
