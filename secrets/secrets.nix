fb9d958c264cdc662c2fc19df93dcc57cbc0c8fe
Rajan Maghera <rmaghera@ualberta.ca>
2024-09-30 23:24:28 -0600

    chore: update secrets for machine pie

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,6 +2,7 @@ let
   keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG8ZCH5zjDnnRouiFA0QrGuygX8mi4EWGj4nsXwQyKQ+ rajan@fruit"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINGWI7xEZ1TeqmHkIusuwREMEZdo2kY48sEBsKIHVyoC sourpi"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBR1uxDllS0fbZCkUebMlTrnvQCroyhrvZfWNVD/5ysQ root@pie"
   ];
   manifest = import ./manifest.nix;
   secretsAttrs = builtins.listToAttrs (builtins.map (x: {
