bc515f3358775b9da1f99672ba9692b80746d279
Rajan Maghera <rmaghera@ualberta.ca>
2024-12-30 12:40:23 -0700

    chore: add self-signed nginx key

--- secrets/default.nix
+++ secrets/default.nix
@@ -2,9 +2,11 @@ let
   manifest = import ./manifest.nix;
   secretsAttrs = builtins.listToAttrs (builtins.map (x: {
       name = x.name;
-      value = {
-        file = ./. + "/${x.name}.age";
-      };
+      value =
+        {
+          file = ./. + "/${x.name}.age";
+        }
+        // (x.extraAttrs or {});
     })
     manifest);
 in {
--- secrets/manifest.nix
+++ secrets/manifest.nix
@@ -3,4 +3,13 @@
     name = "lab-key";
     description = "Homelab/services caddy authentication secret key";
   }
+  {
+    name = "nginx-selfsigned-key";
+    description = "Self-signed certificate key for nginx";
+    extraAttrs = {
+      mode = "770";
+      owner = "nginx";
+      group = "nginx";
+    };
+  }
 ]
