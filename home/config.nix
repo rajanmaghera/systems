637f07682629e94d965b00d704841d2c935d8429
Rajan Maghera <rmaghera@ualberta.ca>
2024-12-30 12:38:31 -0700

    chore: remove fish shell and wallpaper

--- home/config.nix
+++ home/config.nix
@@ -3,8 +3,6 @@
   my.llm.enable = false;
   my.cli.enable = true;
   my.dev-env.enable = true;
-  my.shell.fish.enable = true;
   my.shell.zsh.enable = true;
   my.tools.secrets.enable = true;
-  my.wallpaper.enable = true;
 }
