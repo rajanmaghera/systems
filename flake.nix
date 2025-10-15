fa44c0421fefc9fb6ee59819229147c22c5d16ad
Rajan Maghera <rmaghera@ualberta.ca>
2025-10-15 15:16:17 -0400

    chore: update flake.lock

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     darwin.url = "github:LnL7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
