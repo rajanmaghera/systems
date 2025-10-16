90cf981742ec3092e05af096155fd8ae0d61cbf7
Rajan Maghera <rmaghera@ualberta.ca>
2025-10-16 11:59:07 -0400

    chore: switch back to nixpkgs
    some packages are causing a headache on nixos, somehow nixpkgs is more
    stable for the time being

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     darwin.url = "github:LnL7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
