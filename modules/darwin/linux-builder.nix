ef63bd8fe0dd9178169def9d042dadd09f6a37e8
Rajan Maghera <rmaghera@ualberta.ca>
2025-12-01 16:36:56 -0500

    feat: update linux builder on macos

--- modules/darwin/linux-builder.nix
+++ modules/darwin/linux-builder.nix
@@ -18,6 +18,15 @@ in
   config = mkIf cfg.enable {
     nix.linux-builder = {
       enable = true;
+      config = {
+        virtualisation = {
+          darwin-builder = {
+            diskSize = 200 * 1024;
+            memorySize = 8 * 1024;
+          };
+          cores = 6;
+        };
+      };
     };
   };
 }
