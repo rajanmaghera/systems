c4be04601270b369efe40bed592cd098f5bfc5e1
Rajan Maghera <rmaghera@ualberta.ca>
2025-12-04 13:57:23 -0500

    feat: update llvm-shell

--- pkgs/llvm-shell/default.nix
+++ pkgs/llvm-shell/default.nix
@@ -1,33 +1,52 @@
 {
-  lib,
   mkShell,
   llvmPackages_latest,
   cmake,
   ninja,
   zlib,
+  zstd,
   python3,
   bashInteractive,
   ccache,
-  libgccjit,
-  jetbrains,
+  pkg-config,
+  apple-sdk,
+  lib,
+  writeShellScriptBin,
+  stdenv,
   ...
 }:
-with lib;
-mkShell {
+let
+  # Hack to fix clang not picking up on the right packages
+  clangd-wrapped = writeShellScriptBin "clangd" ''
+    export CPLUS_INCLUDE_PATH="${llvmPackages_latest.libcxx.dev}/include/c++/v1:$CPLUS_INCLUDE_PATH"
+    exec ${llvmPackages_latest.clang-tools}/bin/clangd "$@"
+  '';
+in
+mkShell.override { stdenv = llvmPackages_latest.stdenv; } {
 
-  buildInputs = [
-    llvmPackages_latest.llvm
+  nativeBuildInputs = [
+    clangd-wrapped
     cmake
     ninja
+    pkg-config
+    ccache
+  ]
+  ++ lib.optionals stdenv.isDarwin [
+    apple-sdk
+  ];
+
+  buildInputs = [
     python3
     bashInteractive
-    ccache
     zlib
-    llvmPackages_latest.clang
+    zstd
+    llvmPackages_latest.llvm
     llvmPackages_latest.lld
-    llvmPackages_latest.libcxx
-    llvmPackages_latest.libunwind
-    jetbrains.clion
-    libgccjit
+    llvmPackages_latest.lldb
   ];
+
+  shellHook = ''
+    # Zlib needs special re-importing
+    export NIX_LDFLAGS="-rpath ${zlib}/lib $NIX_LDFLAGS"
+  '';
 }
