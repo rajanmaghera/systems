f486a8ec66a6cea5c3e404d5156c5532e9eeed6b
Rajan Maghera <rmaghera@ualberta.ca>
2026-04-01 13:38:15 -0400

    chore: remove temporary config for broken electron

--- mods/passwords.nix
+++ mods/passwords.nix
@@ -1,64 +1,13 @@
 {
-
-  conf.modTb.home.broken-electron = {
-    message = "Electron fails to build, causing Bitwarden to fail. Should be auto-fixed soon";
-    warnDate = 20260401;
-    abortDate = 20260415;
-    tempConfig =
-      {
-        config,
-        lib,
-        pkgs,
-        ...
-      }:
-      let
-        cfg = config.my.passwords;
-      in
-      {
-        config = lib.mkIf cfg.enable {
-          home.packages =
-            if pkgs.stdenv.isLinux then
-              [ ]
-            else
-              with pkgs;
-              [
-                bitwarden-cli
-                bitwarden-desktop
-              ];
-        };
-      };
-
-    finalConfig =
-      {
-        config,
-        lib,
-        pkgs,
-        ...
-      }:
-      let
-        cfg = config.my.passwords;
-      in
-      {
-        config = lib.mkIf cfg.enable {
-          home.packages = with pkgs; [
-            bitwarden-cli
-            bitwarden-desktop
-          ];
-        };
-      };
-
-  };
-
   mods.home.my.passwords =
     {
       pkgs,
       ...
     }:
     {
-      # home.packages = with pkgs; [
-      #   bitwarden-cli
-      #   bitwarden-desktop
-      # ];
-
+      home.packages = with pkgs; [
+        bitwarden-cli
+        bitwarden-desktop
+      ];
     };
 }
