71d258439738420e8153c4361a9bb6f01b4356d3
Rajan Maghera <rmaghera@ualberta.ca>
2026-04-20 17:38:13 -0400

    fix: add disk configs for new machines

--- mods/titan.nix
+++ mods/titan.nix
@@ -4,6 +4,7 @@
 
   sys.titan.mod =
     {
+      lib,
       ...
     }:
     {
@@ -30,6 +31,61 @@
       my.immich.enable = true;
       my.home-assistant.enable = true;
       my.samba.enable = true;
+
+      # DISK CONFIG
+
+      disko.devices = {
+        disk.disk1 = {
+          device = lib.mkDefault "/dev/sda";
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions = {
+              boot = {
+                name = "boot";
+                size = "1M";
+                type = "EF02";
+              };
+              esp = {
+                name = "ESP";
+                size = "500M";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                };
+              };
+              root = {
+                name = "root";
+                size = "100%";
+                content = {
+                  type = "lvm_pv";
+                  vg = "pool";
+                };
+              };
+            };
+          };
+        };
+        lvm_vg = {
+          pool = {
+            type = "lvm_vg";
+            lvs = {
+              root = {
+                size = "100%FREE";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                  mountOptions = [
+                    "defaults"
+                  ];
+                };
+              };
+            };
+          };
+        };
+      };
 
     };
 }
