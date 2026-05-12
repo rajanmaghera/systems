74903bdc5b016a153c0c50fdac0254d38bbf310e
Rajan Maghera <rmaghera@ualberta.ca>
2026-05-12 16:51:55 -0400

    chore: add impermanence and update flake

--- flake.nix
+++ flake.nix
@@ -40,6 +40,9 @@
     plasma-manager.inputs.home-manager.follows = "home-manager";
     vscode-server.url = "github:nix-community/nixos-vscode-server";
     vscode-server.inputs.nixpkgs.follows = "nixpkgs";
+    impermanence.url = "github:nix-community/impermanence";
+    impermanence.inputs.nixpkgs.follows = "";
+    impermanence.inputs.home-manager.follows = "";
   };
 
   outputs =
