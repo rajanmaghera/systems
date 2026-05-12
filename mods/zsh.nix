4e6aef9a20a0df45ed519255d4a9fa1072a47ff9
Rajan Maghera <rmaghera@ualberta.ca>
2026-05-12 18:09:17 -0400

    feat: add ze and gssh commands

--- mods/zsh.nix
+++ mods/zsh.nix
@@ -10,7 +10,7 @@
     {
       home.shellAliases = {
         "sw" =
-          lib.mkIf pkgs.stdenv.isDarwin "sudo darwin-rebuild switch --flake ~/systems#fruit --show-trace";
+          lib.mkIf pkgs.stdenv.isDarwin "sudo darwin-rebuild switch --flake ~/Projects/systems#fruit --show-trace";
       };
 
       programs.zsh = {
@@ -27,6 +27,32 @@
           # Bind standard terminal Alt-Left/Right to word jumping
           bindkey "^[[1;3D" backward-word
           bindkey "^[[1;3C" forward-word
+
+
+          # Shell function for quickly launching editor layout
+          ze() {
+            if [[ -z "$1" ]]; then
+              echo "Usage: ze <project-folder>"
+              return 1
+            fi
+            
+            zellij action new-tab --layout "$HOME/editor.kdl" --cwd "$HOME/Projects/$1" --name "editor/$1"
+          }
+          _ze_completion() {
+            _arguments '1:project:_files -W "$HOME/Projects" -/'
+          }
+          compdef _ze_completion ze
+
+          # Shell function for connecting to any of my remotes' zellij instances
+          gssh() {
+            if [[ -z "$1" ]]; then
+              echo "Usage: gssh <ssh-host>"
+              return 1
+            fi
+            
+            ghostty -e ssh -t "$1" "zsh -l -c 'zellij attach -c main'" >/dev/null 2>&1 &
+          }
+          compdef gssh=ssh
         '';
       };
 
